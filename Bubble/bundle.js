218ab50b6c6f69b23155d2b749b34b74f394b432
Cecilia Rådegran <Cecilia@Cecilias-MacBook-Pro.local>
2018-06-26 11:49:59 +0200

    bundle

---
+++ Bubble/bundle.js
@@ -0,0 +1,277 @@
+var colors = [
+    "#ffff00",
+    "#ff80d5",
+    "#ff0000",
+    "#00b33c",
+    "#cc00ff",
+    "#66ccff",
+    "#8cff66",
+];
+var shootSound = new buzz.sound("http://soundbible.com/grab.php?id=930&type=mp3");
+var fallBubbleSound = new buzz.sound("http://soundbible.com/grab.php?id=85&type=mp3");
+var getRandomColor = function () {
+    return colors[Math.floor(Math.random() * colors.length)];
+};
+var Bubble = function (point, radius, fill) {
+    var s = new paper.Path.Circle(point.clone().add([4, 4]), radius);
+    var shape = new paper.Path.Circle(point.clone(), radius);
+    var fillColor = fill || getRandomColor();
+    var group = new paper.Group({
+        children: [s, shape]
+    });
+    group.fillColor = fillColor;
+    group.translate(new paper.Point(-2, -2));
+    s.fillColor = "black";
+    return {
+        shape: group,
+        color: fillColor,
+        vx: 0,
+        vy: 0
+    };
+};
+// let debug = function(id, p, r, fillColor)
+// {
+//     if (!debug.elem) { debug.elem = {}; }
+//     if (!debug.elem[id]) { debug.elem[id] = Bubble(p.x, p.y, r, fillColor); }
+//     debug.elem[id].shape.position = new paper.Point(p.x, p.y);
+// };
+// let HexGrid = function(origo, radius)
+// {
+//     let pointToHex = function(point)
+//     {
+//         let hexY = Math.round((point.y - boardTop - radius) / (Math.sqrt(3)*radius) );
+//         let isOdd = (hexY % 2 == 1);
+//         let hexX = Math.round((point.x - boardLeft - radius - (isOdd ? radius : 0)) / (2*radius));
+//         return {"x": hexX, "y": hexY};
+//     };
+//     let hexToPoint = function(hex)
+//     {
+//         let isOdd = (100+hex.y) % 2 == 1;
+//         let x = boardLeft + radius + (2*radius*hex.x) + (isOdd ? radius : 0);
+//         let y = boardTop + radius + (Math.sqrt(3)*radius*hex.y);
+//         return {"x": x, "y": y};
+//     };
+// };
+var Game = function (canvas) {
+    paper.install(window);
+    paper.setup(canvas);
+    var view = paper.view;
+    var canvasHeight = window.innerHeight;
+    var ballSpeed = 12;
+    var radius = canvasHeight / 40;
+    var ballsPerRow = 10;
+    var ballsPerColumn = 12;
+    var boardWidth = ballsPerRow * (radius * 2);
+    var boardTop = 30;
+    var boardLeft = 20;
+    var hexGrid = [];
+    var pointToHex = function (point) {
+        var hexY = Math.round((point.y - boardTop - radius) / (Math.sqrt(3) * radius));
+        var isOdd = (hexY % 2 == 1);
+        var hexX = Math.round((point.x - boardLeft - radius - (isOdd ? radius : 0)) / (2 * radius));
+        return { "x": hexX, "y": hexY };
+    };
+    var hexToPoint = function (hex) {
+        var isOdd = (100 + hex.y) % 2 == 1;
+        var x = boardLeft + radius + (2 * radius * hex.x) + (isOdd ? radius : 0);
+        var y = boardTop + radius + (Math.sqrt(3) * radius * hex.y);
+        return new paper.Point(x, y);
+    };
+    var adjacentHex = function (hexX, hexY) {
+        var center = hexToPoint({ x: hexX, y: hexY });
+        var list = [];
+        for (var i = 0; i < 6; i++) {
+            var x = center.x + 2 * radius * Math.cos(2 * 3.1415 * i / 6);
+            var y = center.y + 2 * radius * Math.sin(2 * 3.1415 * i / 6);
+            list.push(pointToHex(new paper.Point(x, y)));
+        }
+        return list;
+    };
+    for (var row = 0; row < ballsPerColumn; row++) {
+        var r = [];
+        var isOdd = row % 2 == 1;
+        var ballsInThisRow = ballsPerRow - (isOdd ? 1 : 0);
+        for (var col = 0; col < ballsInThisRow; col++) {
+            var p = hexToPoint({ x: col, y: row });
+            var bubble = Bubble(p, radius);
+            r.push(bubble);
+        }
+        hexGrid.push(r);
+    }
+    var playerBall = Bubble(new paper.Point(boardLeft + ballsPerRow * radius, canvasHeight - 3 * radius), radius);
+    var fixZIndex = function () {
+        var current = null;
+        for (var i = 0; i < hexGrid.length; i++) {
+            for (var j = 0; j < hexGrid[i].length; j++) {
+                var b = hexGrid[i][j];
+                if (b && current) {
+                    b.shape.insertAbove(current.shape);
+                }
+                current = b;
+            }
+        }
+    };
+    var shooting = false;
+    view.onMouseDown = function (event) {
+        if (!shooting) {
+            shootSound.stop();
+            shootSound.play();
+            shooting = true;
+            var vx = event.point.x - playerBall.shape.position.x;
+            var vy = event.point.y - playerBall.shape.position.y;
+            var dist = Math.sqrt(vx * vx + vy * vy) / ballSpeed;
+            playerBall.vx = vx / dist;
+            playerBall.vy = vy / dist;
+        }
+    };
+    var getBubble = function (hexX, hexY) {
+        var row = hexGrid[hexY];
+        if (row) {
+            return row[hexX];
+        }
+        return null;
+    };
+    var setBubble = function (bubble, hexX, hexY) {
+        if (hexX < 0 || hexY < 0) {
+            throw "no can do";
+        }
+        var row = hexGrid[hexY];
+        while (!row) {
+            hexGrid.push([]);
+            row = hexGrid[hexY];
+        }
+        var col = row[hexX];
+        while (col !== null) {
+            row.push(null);
+            col = row[hexX];
+        }
+        hexGrid[hexY][hexX] = bubble;
+    };
+    var fallingBubbles = [];
+    // Returns list of connected bubbles in hexCoordinates
+    var findConnected = function (startHex, predicate) {
+        var thiskey = JSON.stringify(startHex);
+        var counted = {};
+        counted[thiskey] = true;
+        var recurse = function (hx, hy) {
+            var list = adjacentHex(hx, hy);
+            for (var i = 0; i < list.length; i++) {
+                var b = getBubble(list[i].x, list[i].y);
+                var key = JSON.stringify(list[i]);
+                if (b && !counted[key] && predicate(b)) {
+                    // connected, same color, not counted!
+                    counted[key] = true;
+                    recurse(list[i].x, list[i].y);
+                }
+            }
+        };
+        recurse(startHex.x, startHex.y);
+        return Object.keys(counted).map(function (key) { return JSON.parse(key); });
+    };
+    var tryKillBubbles = function (hexX, hexY) {
+        var hexP = { x: hexX, y: hexY };
+        var thisColorString = getBubble(hexX, hexY).color.toString();
+        var sameColorPredicate = function (bubble) {
+            return bubble.color.toString() == thisColorString;
+        };
+        // Kill all with same color
+        var connected = findConnected(hexP, sameColorPredicate);
+        if (connected.length > 2) {
+            fallBubbleSound.stop();
+            fallBubbleSound.play();
+            var shootingBubble = getBubble(hexX, hexY);
+            for (var i = 0; i < connected.length; i++) {
+                var hexP_1 = connected[i];
+                var bubble = getBubble(hexP_1.x, hexP_1.y);
+                bubble.shape.bringToFront();
+                bubble.vx = shootingBubble.vx;
+                bubble.vy = shootingBubble.vy;
+                fallingBubbles.push(bubble);
+                hexGrid[hexP_1.y][hexP_1.x] = null;
+            }
+            // Kill all dangling
+            var topRow = hexGrid[0];
+            var connectedDict_1 = {};
+            for (var i = 0; i < topRow.length; i++) {
+                //let topBubble = getBubble(i, 0);
+                if (getBubble(i, 0)) {
+                    findConnected({ x: i, y: 0 }, function () { return true; })
+                        .map(function (hexP) { connectedDict_1[JSON.stringify(hexP)] = true; });
+                }
+            }
+            for (var i = 0; i < hexGrid.length; i++) {
+                for (var j = 0; j < hexGrid[i].length; j++) {
+                    var hexP_2 = { x: j, y: i };
+                    if (!connectedDict_1[JSON.stringify(hexP_2)]) {
+                        var bubble = hexGrid[i][j];
+                        if (bubble) {
+                            // This is dangling!            
+                            bubble.vx = shootingBubble.vx;
+                            bubble.vy = shootingBubble.vy * 0;
+                            fallingBubbles.push(bubble);
+                            hexGrid[hexP_2.y][hexP_2.x] = null;
+                        }
+                    }
+                }
+            }
+        }
+    };
+    view.onFrame = function (event) {
+        var now = Date.now();
+        for (var i = 0; i < fallingBubbles.length; i++) {
+            var f = fallingBubbles[i];
+            f.startFallTime = f.startFallTime || now;
+            if (now - f.startFallTime > 5000) {
+                f.shape.remove();
+                fallingBubbles.splice(i, 1);
+                i--;
+                continue;
+            }
+            var p = f.shape.position;
+            f.shape.position = new paper.Point(p.x + f.vx, p.y + f.vy);
+            f.vy += 0.5;
+        }
+        if (shooting) {
+            // update velocity
+            var p = playerBall.shape.position;
+            playerBall.shape.position = new paper.Point(p.x + playerBall.vx, p.y + playerBall.vy);
+            p = playerBall.shape.position;
+            if (p.x < (boardLeft + radius) || p.x > (boardLeft + boardWidth - radius)) {
+                playerBall.vx = -playerBall.vx;
+            }
+            // collision test
+            var point = playerBall.shape.position;
+            var hexPoint = pointToHex(point);
+            var hexX = hexPoint.x;
+            var hexY = hexPoint.y;
+            for (var i = hexY - 1; i <= hexY + 1; i++) {
+                for (var j = hexX - 1; j <= hexX + 1; j++) {
+                    //debug("hex" + (j-hexX) + "," + (i - hexY), hexToPoint({x:hexX,y:hexY}), radius*0.8, "red");
+                    var bubble = getBubble(j, i);
+                    if (bubble) {
+                        var bp = bubble.shape.position;
+                        var dist = Math.sqrt((bp.x - point.x) * (bp.x - point.x) + (bp.y - point.y) * (bp.y - point.y));
+                        if (dist < 2 * radius * 0.9) {
+                            // Collision!
+                            setBubble(playerBall, hexX, hexY);
+                            var quant = hexToPoint(hexPoint);
+                            var newp = new paper.Point(quant.x, quant.y);
+                            playerBall.shape.position = newp;
+                            playerBall = Bubble(new paper.Point(boardLeft + ballsPerRow * radius, canvasHeight - 3 * radius), radius);
+                            shooting = false;
+                            fixZIndex();
+                            tryKillBubbles(hexX, hexY);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    };
+    var start = function () {
+    };
+    return {
+        "start": start
+    };
+};
+//# sourceMappingURL=bundle.js.map
